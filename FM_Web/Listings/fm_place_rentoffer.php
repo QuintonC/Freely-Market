a5b74398248edb601db9fd4223872cf70856e84c
[Padraic] <[pekirby@umail.iu.edu]>
2017-02-02 11:44:48 -0500

    Notifications now work for reject offers

--- FM_Web/Listings/fm_place_rentoffer.php
+++ FM_Web/Listings/fm_place_rentoffer.php
@@ -15,14 +15,34 @@ $username = $_SESSION['username'];
 
 #Get from url
 $rid = $_GET['id'];
-#Create offer for rental
-$sql = "insert into Pending_Rental (username,rid) values ('$username','$rid')";
 
-if ($conn->query($sql) === TRUE) {
+#Create offer for rental
+$mysql = "insert into Pending_Rental (username,rid) values ('$username','$rid')";
+$conn->query($mysql);
+
+$place = "You have recieved an offer";
+$date = date("Y-m-d H:i:s");
+
+$sql = "select prid from Pending_Rental where username = '$username' and rid = '$rid' limit 1";
+$content = $conn->query($sql);
+$row = mysqli_fetch_array($content);
+$prid = $row['prid'];
+
+$sql2 = "SELECT u.username FROM User_Accounts AS u, Rental_Listing AS r, Pending_Rental AS p WHERE p.rid = r.rid AND r.aid = u.aid AND prid = '$prid'";
+$data = $conn->query($sql2);
+$set = mysqli_fetch_array($data);
+$seller = $set['username'];
+
+#Create notification
+
+$sql3 = "INSERT INTO Notifications(recipient,sender,types,created,prid) VALUES('$seller','$username','$place','$date','$prid')";
+
+if ($conn->query($sql3) === TRUE) {
 	header("Location: fm_account.php");
 	exit;
 } else {
-	echo "Error: " . $sql . "<br>" . $conn->error;
+	echo "Error: " . $sql3 . "<br>" . $conn->error;
 }
+
 
 ?>
