0a0bf1f11668bb0dc672ec78ddea6c40b2983f42
Quinton Chester <quintonchester@gmail.com>
2017-04-12 21:56:06 -0400

    fixing master branch for app dependencies (Austin + Quinton)

--- Web/includes/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST', 'db.soic.indiana.edu'); 
-define('DB_USERNAME', 'caps16_team12');
-define('DB_PASSWORD', 'my+sql=caps16_team12');
-define('DB_NAME', 'caps16_team12');
-
-?>
